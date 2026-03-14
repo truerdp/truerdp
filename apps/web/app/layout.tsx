5479470211ef7ac2872db85478b44c2b009c3d21
Ujjawal Chatterjee <yourdevujjawal@gmail.com>
2026-03-14 11:47:43 +0530

    feat: initial commit

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,30 @@
+import { Geist, Geist_Mono, Roboto } from "next/font/google"
+
+import "@workspace/ui/globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+import { cn } from "@workspace/ui/lib/utils";
+
+const roboto = Roboto({subsets:['latin'],variable:'--font-sans'})
+
+const fontMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn("antialiased", fontMono.variable, "font-sans", roboto.variable)}
+    >
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  )
+}
